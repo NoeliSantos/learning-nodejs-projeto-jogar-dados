680f8520e2f20a4ca84f7b182e6113dad5530d2f
Your Name <you@example.com>
2019-06-06 12:27:11 -0300

    adicionadas funcionalidades de encerrar e reiniciar. tbm iniciamos bootstrap

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -8,13 +8,40 @@ Responsável por:
 - Pedir ao model para iniciar um novo jogo
 */
 
-var model = require('../models/jogo');
+//var model = require('../models/jogo');
 var jogoModel = undefined;
 
 module.exports.iniciar =function (application,req,res){
 	console.log('controller: iniciar'); 
     console.log('controller: cria instância de jogo');  
-    jogoModel =  new model.Jogo();
+    jogoModel =  new application.app.models.jogo.Jogo();
     console.log('controller: atualiza view - novoJogo');    
     res.render('novoJogo');
+}
+module.exports.novoLancamento = function(application,req,res){
+	console.log('controller:novoLancamento');
+	if (jogoModel){
+		console.log('controller:pede para o model fazer novoLancamento')
+		var resultado = jogoModel.novoLancamento();
+		res.render('novoLancamento',resultado);
+	}
+}
+module.exports.reiniciar = function(application,req,res){
+	console.log('controller:reiniciar');
+    if(jogoModel){
+    	console.log('controller:pede para o model criar um novo jogo');
+    	jogoModel.novoJogo();
+    	console.log('controller:atualizar a view');
+    	res.render('novoJogo');
+
+    }else{
+        console.log('controller:atualizar a view para pagina principal');
+        	res.render('home');
+        }
+}
+
+     module.exports.encerrar = function(application,req,res){
+     	console.log('controller:encerrar');
+     	jogoModel = undefined;
+     	res.render('home');
 }
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -11,16 +11,18 @@ module.exports = function(application){
 
 	application.get('/lancarDados', function(req,res){
     	console.log('routes: /lancarDados');
-    	res.send('Esta funcionalidade não foi implementada ainda.'); 
+    	application.app.controllers.jogo.novoLancamento(application,req,res);
     });
 
     application.get('/reiniciar', function(req,res){
-    	console.log('routes: /reiniciar');    	        
+    	console.log('routes: /reiniciar'); 
+    	application.app.controllers.jogo.reiniciar(application,req,res);   	        
     	res.send('Esta funcionalidade não foi implementada ainda.');
     });
     
     application.get('/encerrar', function(req,res){
-    	console.log('routes: /encerrar');        
+    	console.log('routes: /encerrar'); 
+    	application.app.controllers.jogo.encerrar(application,req,res);      
     	res.send('Esta funcionalidade não foi implementada ainda.');
     });
 
